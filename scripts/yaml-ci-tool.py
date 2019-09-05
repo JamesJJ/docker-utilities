ac686d1aedf52d9d2b69914669bd94123ac3772b
JamesJJ <jj@fcg.fyi>
2019-09-05 10:11:30 +0800

    Support "CronJob"

--- scripts/yaml-ci-tool.py
+++ scripts/yaml-ci-tool.py
@@ -8,7 +8,7 @@ image_new = sys.argv[2]
 
 # TODO: input validation
 # TODO: check for file exist
-# TODO: handle other than 'deployment'
+# TODO: handle other than 'deployment' / 'CronJob'
 # TODO: let the match be a regex match, instead of the escaped match (safely)
 
 with YAML(output=sys.stdout) as yaml:
@@ -20,4 +20,9 @@ with YAML(output=sys.stdout) as yaml:
                 for i,c in enumerate(data['spec']['template']['spec']['containers']):
                     if re.search(re.escape(image_match), c['image']):
                         data['spec']['template']['spec']['containers'][i]['image'] = image_new
+            if (data['kind'] == 'CronJob'):
+                for i,c in enumerate(data['spec']['jobTemplate']['spec']['template']['spec']['containers']):
+                    if re.search(re.escape(image_match), c['image']):
+                        data['spec']['jobTemplate']['spec']['template']['spec']['containers'][i]['image'] = image_new
             yaml.dump(data)
+
