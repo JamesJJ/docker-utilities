7bacd28fa0a550a1938840332f2172cf6610c9a8
JamesJJ <jj@fcg.fyi>
2019-12-01 22:19:20 +0800

    Support anchored regex match

--- scripts/yaml-ci-tool.py
+++ scripts/yaml-ci-tool.py
@@ -6,10 +6,13 @@ from pprint import pprint as pp
 image_match = sys.argv[1]
 image_new = sys.argv[2]
 
+# Regex matches must be anchored, otherwise take it as a fixed string match
+if (not image_match.startswith('^') and not image_match.endswith('$')):
+    image_match = re.escape(image_match)
+
 # TODO: input validation
 # TODO: check for file exist
 # TODO: handle other than 'deployment' / 'CronJob'
-# TODO: let the match be a regex match, instead of the escaped match (safely)
 
 with YAML(output=sys.stdout) as yaml:
     yaml.explicit_start = True
@@ -19,11 +22,11 @@ with YAML(output=sys.stdout) as yaml:
         if data is not None:
             if (data['kind'] == 'Deployment'):
                 for i,c in enumerate(data['spec']['template']['spec']['containers']):
-                    if re.search(re.escape(image_match), c['image']):
+                    if re.search(image_match, c['image']):
                         data['spec']['template']['spec']['containers'][i]['image'] = image_new
             if (data['kind'] == 'CronJob'):
                 for i,c in enumerate(data['spec']['jobTemplate']['spec']['template']['spec']['containers']):
-                    if re.search(re.escape(image_match), c['image']):
+                    if re.search(image_match, c['image']):
                         data['spec']['jobTemplate']['spec']['template']['spec']['containers'][i]['image'] = image_new
             yaml.dump(data)
 
